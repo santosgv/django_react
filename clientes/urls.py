66ffc5bc91b099b3da01622eafb20cb3e2905208
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2023-03-24 19:50:04 -0300

    simple jwt

--- clientes/urls.py
+++ clientes/urls.py
@@ -5,8 +5,16 @@ from clientes import views
 
 app_name='CLIENTE'
 
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
 urlpatterns = [
-    path('cliente/',views.ClienteListAPIView.as_view(),
-         name='cliente-list'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('cliente/',views.ClienteListAPIView.as_view(),name='cliente-list'),
 
 ]
--- djangoreact/settings.py
+++ djangoreact/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+     'rest_framework_simplejwt',
     'corsheaders',
     'clientes',
 ]
@@ -92,6 +93,15 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+ 
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+    
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+
+}
 
 
 # Internationalization
